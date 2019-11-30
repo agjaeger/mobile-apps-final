2d460e6c958a371f952836bd323ec1e02cfed35a
STUDENTS\agjaeger <agjaeger@ualr.edu>
2019-11-30 17:36:08 -0600

    added test for key-value database

--- idlegame/app/src/main/java/com/example/idlegame/MainActivity.java
+++ idlegame/app/src/main/java/com/example/idlegame/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.idlegame.tasks.UpdateProgressBarsTask;
+import com.snappydb.DB;
+import com.snappydb.DBFactory;
+import com.snappydb.SnappydbException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +26,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        new UpdateProgressBarsTask().execute();
+        // new UpdateProgressBarsTask().execute();
+
+
+        try {
+            DB snappydb = DBFactory.open(getApplicationContext());
+
+            if (snappydb.exists("myint")) {
+                int myint = snappydb.getInt("myint");
+
+                snappydb.putInt("myint", myint + 1);
+            } else {
+                snappydb.putInt("myint", 0);
+            }
+
+            System.out.println(snappydb.getInt("myint"));
+
+            snappydb.close();
+        } catch (SnappydbException sde) {
+            System.out.println("SNAPPY EXCEPTION");
+            System.out.println(sde);
+        };
 
     }
 }
