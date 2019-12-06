966169dd1c410f19b2248ee65d41a60c77df90c8
TreyClark <47194665+TreyClark@users.noreply.github.com>
2019-12-06 16:06:59 -0600

    Upgrades_tab working
    Finished the upgrades tab boilerplate work

--- idlegame/app/src/main/java/com/ualr/idlegame/fragments/tabs/UpgradesTabFragment.java
+++ idlegame/app/src/main/java/com/ualr/idlegame/fragments/tabs/UpgradesTabFragment.java
@@ -4,12 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.ualr.idlegame.R;
+import com.ualr.idlegame.fragments.UpgradeRowFragment;
 import com.ualr.idlegame.fragments.interfaces.TabFragment;
 import com.ualr.idlegame.viewmodel.AppDataViewModel;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class UpgradesTabFragment extends Fragment implements TabFragment {
     private UpgradesTabFragment.UpgradesTabFragmentViewHolder viewHolder = null;
@@ -46,8 +54,37 @@ public class UpgradesTabFragment extends Fragment implements TabFragment {
 
 
     public class UpgradesTabFragmentViewHolder {
+        private LinearLayout linearLayout;
+
+        private List<UpgradeRowFragment> upgradeRowFragments = new ArrayList();
+
 
         public UpgradesTabFragmentViewHolder(View view){
+            linearLayout = view.findViewById(R.id.upgrade_linear_layout);
+
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+
+
+            upgradeRowFragments.add(constructUpgradeRowFragment("10", "bonus 1", "x2"));
+            ft.add(linearLayout.getId(), upgradeRowFragments.get(upgradeRowFragments.size() - 1), getRandomKey());
+        }
+
+        private UpgradeRowFragment constructUpgradeRowFragment (String costLabel, String typeLabel, String valueLabel) {
+            Bundle bundle = new Bundle();
+            bundle.putString("costLabel", costLabel);
+            bundle.putString("typeLabel", typeLabel);
+            bundle.putString("valueLabel", valueLabel);
+
+            UpgradeRowFragment arf = new UpgradeRowFragment();
+            arf.setArguments(bundle);
+
+            return arf;
+        }
+
+        private String getRandomKey () {
+            byte[] array = new byte[7]; // length is bounded by 7
+            new Random().nextBytes(array);
+            return new String(array, Charset.forName("UTF-8"));
         }
 
     }
