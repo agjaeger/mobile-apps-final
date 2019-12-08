7d8f3bb064e94e6fcd246e52b717d54c0cd5e74d
TreyClark <47194665+TreyClark@users.noreply.github.com>
2019-12-07 19:19:13 -0600

    Trying to finish this up

--- idlegame/app/src/main/java/com/ualr/idlegame/fragments/tabs/MapTabFragment.java
+++ idlegame/app/src/main/java/com/ualr/idlegame/fragments/tabs/MapTabFragment.java
@@ -8,19 +8,27 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProviders;
 
 import com.ualr.idlegame.R;
 import com.ualr.idlegame.fragments.interfaces.TabFragment;
+import com.ualr.idlegame.viewmodel.AppDataViewModel;
 
 public class MapTabFragment extends Fragment implements TabFragment {
 
     private MapTabFragmentViewHolder viewHolder = null;
+    private AppDataViewModel viewModel;
     private boolean mActive = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.tab_map_fragment, container, false);
+
+        viewModel = ViewModelProviders.of(getActivity()).get(AppDataViewModel.class);
+
         viewHolder = new MapTabFragmentViewHolder(view);
+
 
         return view;
         //return inflater.inflate(R.layout.tab_map_fragment, container, false);
@@ -48,15 +56,29 @@ public class MapTabFragment extends Fragment implements TabFragment {
 
     public class MapTabFragmentViewHolder {
         private ImageView imageView;
+        private LinearLayout customButton;
+        private ImageView blankSpace;
 
         public MapTabFragmentViewHolder(View view){
             imageView = view.findViewById(R.id.map_placeholder);
+
             imageView.setOnClickListener(new View.OnClickListener(){
                 @Override
                 public void onClick(View v){
-                    setImage(R.drawable.map2);
-                }
+                    System.out.println(viewModel.getResourceValue("Power"));
+                    //if(viewModel.getResourceValue("Power") >= 1000) {
+                    //    setImage(R.drawable.map4);
+                    //}
+                    /*else if (viewModel.getResourceValue("Power") >= 1000) {
+                        setImage(R.drawable.map3);
+                    }
+                    else if (viewModel.getResourceValue("Power") >= 1000) {
+                        setImage(R.drawable.map2);
+                    }*/
+            }
+
             });
+
         }
 
         public void setImage(int imageID) {
